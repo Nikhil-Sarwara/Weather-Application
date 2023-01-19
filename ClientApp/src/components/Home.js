8a5129e733b913cafeb6a6616e324b38a3b5ca5d
Nikhil <84772872+tymnec@users.noreply.github.com>
2023-01-19 11:12:31 +0530

    finish frontend displaying cities

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,19 +1,67 @@
 import React, {Component} from 'react';
-import {Button, Flex} from "@chakra-ui/react";
+import {Flex, Text} from "@chakra-ui/react";
 import {UserProfile} from "./UserProfile";
+import {SearchBar} from "./SearchBar";
+import {SearchContext, SearchContextProvider} from "./SearchContextProvider";
+import {CityBox} from "./CityBox";
 
 export class Home extends Component {
     static displayName = Home.name;
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayText: 'Welcome to Weather App!',
+        };
+    }
+
     render() {
         return (
-            <Flex bgColor={"aliceblue"} width={"98vw"} height={"98vh"} marginX={"1vw"} marginY={"1vh"} rounded={"3xl"} padding={"2rem"}>
-               <Flex bgColor={"gray.800"} rounded={"3xl"} padding={"2rem"} width={"100%"} height={"100%"}>
-                   {/* User Profile*/}
-                   <UserProfile />
-                   <Button colorScheme={"teal"}>Hello World!</Button>
-               </Flex> 
-            </Flex>
+            <SearchContextProvider>
+                <SearchContext.Consumer>
+                    {(context) => {
+                        return (
+                            <Flex bgColor={"aliceblue"} width={"98vw"} height={"98vh"} marginX={"1vw"} marginY={"1vh"}
+                                  rounded={"3xl"} padding={"2rem"}>
+                                <Flex bgColor={"gray.800"} rounded={"3xl"} padding={"2rem"} width={"100%"}
+                                      height={"100%"}
+                                      flexDirection={"column"}>
+                                    {/* Top Bar */}
+                                    <Flex justifyContent={"space-between"} width={"100%"}>
+                                        {/* User Profile*/}
+                                        <UserProfile/>
+
+                                        {/* Search Bar*/}
+                                        <SearchBar/>
+                                    </Flex>
+
+                                    {/* Main Display */}
+                                    <Flex height={"100%"} width={"100%"} bgColor={"gray.700"} marginTop={"2rem"}
+                                          rounded={"3xl"} overflow={"auto"}>
+                                        {/* Display Text */}
+                                        {context.searchQuery === "None" ? 
+                                            <Text color={"white"} fontSize={"4rem"} fontWeight={"thin"} margin={"auto"}>
+                                                {this.state.displayText}
+                                            </Text>
+                                             :
+                                            (Array.isArray(context.searchResult) && context.searchResult.length > 0 ?
+                                                    (<Flex>
+                                                        <CityBox cities={context.searchResult}/>
+                                                    </Flex>)
+                                                    :
+                                                    <Text color={"white"} fontSize={"2rem"} fontWeight={"thin"} margin={"auto"}>
+                                                        No results found.
+                                                    </Text>
+                                            )
+                                        }
+                                    </Flex>
+                                </Flex>
+                            </Flex>
+                        )
+                    }}
+                </SearchContext.Consumer>
+            </SearchContextProvider>
         );
     }
+
 }
