cb245af4db2c51150b60ff9c060fcab63eb79fa1
Nikhil <84772872+tymnec@users.noreply.github.com>
2023-01-19 11:12:16 +0530

    add citybox class component for display cities

---
+++ ClientApp/src/components/CityBox.js
@@ -0,0 +1,37 @@
+import {Flex, Text, VStack} from "@chakra-ui/react";
+import React, {Component} from "react";
+import {FaMapMarkerAlt} from "react-icons/fa";
+
+export class CityBox extends Component {
+    
+    render() {
+        return(
+            <Flex justifyContent="center" padding={"1rem"} overflow={"auto"}>
+                <VStack spacing={4} alignItems="stretch">
+                    {this.props.cities.map((city) => (
+                        <Flex
+                            key={city.id}
+                            alignItems="center"
+                            justifyContent="space-between"
+                            backgroundColor="gray.600"
+                            color="white"
+                            width="100%"
+                            padding="1rem"
+                            rounded="3xl"
+                            boxShadow="md"
+                        >
+                            <FaMapMarkerAlt size={24} color="white" />
+                            <VStack alignItems="start" spacing={0} flex="1" marginX={"1rem"}>
+                                <Text fontSize="lg" fontWeight="bold">
+                                    {city.name}
+                                </Text>
+                                <Text fontSize="sm">{`${city.region}, ${city.country}`}</Text>
+                            </VStack>
+                            <Text fontSize="sm">{`${city.lat}, ${city.lon}`}</Text>
+                        </Flex>
+                    ))}
+                </VStack>
+            </Flex>
+        )
+    }
+}
