d5b744055ba5c7c54d33f05fe15bb7e9880ab50c
Nikhil <84772872+tymnec@users.noreply.github.com>
2023-01-19 11:11:53 +0530

    add context provider for searched cities

---
+++ ClientApp/src/components/SearchContextProvider.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+const SearchContext = createContext();
+
+const SearchContextProvider = ({ children }) => {
+    const [searchQuery, setSearchQuery] = useState("None");
+    const [searchResult, setSearchResult] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [notFound, setNotFound] = useState(false);
+
+    return (
+        <SearchContext.Provider value={{searchQuery, setSearchQuery, loading, setLoading, searchResult, setSearchResult, notFound, setNotFound}}>
+            {children}
+        </SearchContext.Provider>
+    );
+};
+
+export { SearchContext, SearchContextProvider };
