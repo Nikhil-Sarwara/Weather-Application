e335d42f1e896e498253f9f5760d10dd7ff3fac1
Nikhil <84772872+tymnec@users.noreply.github.com>
2023-01-19 11:10:39 +0530

    add backend api url in proxyʼ

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -6,7 +6,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 const context = [
     "/weatherforecast",
-    "/user"
+    "/user",
+    "/city"
 ];
 
 const onError = (err, req, resp, target) => {
